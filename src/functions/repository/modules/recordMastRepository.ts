ab5621def8488554805d3d994d9c0ea855e83dc4
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-04-21 20:29:17 +0900

    add recordCache

--- src/functions/repository/modules/recordMastRepository.ts
+++ src/functions/repository/modules/recordMastRepository.ts
@@ -1,4 +1,8 @@
 import { DynamoDBRepositoryBase } from '@/driver/dynamodb/dynamoDBRepositoryBase';
 import { DynamoDBRecordMastRepository } from '@/driver/dynamodb/modules/recordMastRepository';
+import { RecordMastRepositoryCacheAdaptor } from 'cleaning-assessment-service-abr'
 
-export const recordMastRepository = new DynamoDBRecordMastRepository(DynamoDBRepositoryBase.MASTER_TABLE_NAME);
+export const recordMastRepository = new RecordMastRepositoryCacheAdaptor(
+    new DynamoDBRecordMastRepository(DynamoDBRepositoryBase.MASTER_TABLE_NAME)
+)
+
