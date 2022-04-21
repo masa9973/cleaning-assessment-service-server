7c0b854eb3b0bcd4458caeaea39e14fd6fc92221
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-04-21 20:34:07 +0900

    fix cache

--- src/functions/repository/modules/recordMastRepository.ts
+++ src/functions/repository/modules/recordMastRepository.ts
@@ -1,8 +1,4 @@
 import { DynamoDBRepositoryBase } from '@/driver/dynamodb/dynamoDBRepositoryBase';
 import { DynamoDBRecordMastRepository } from '@/driver/dynamodb/modules/recordMastRepository';
-import { RecordMastRepositoryCacheAdaptor } from 'cleaning-assessment-service-abr'
 
-export const recordMastRepository = new RecordMastRepositoryCacheAdaptor(
-    new DynamoDBRecordMastRepository(DynamoDBRepositoryBase.MASTER_TABLE_NAME)
-)
-
+export const recordMastRepository = new DynamoDBRecordMastRepository(DynamoDBRepositoryBase.MASTER_TABLE_NAME)
